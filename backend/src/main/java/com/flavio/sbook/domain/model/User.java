312b09a3e40df974c4bf77d5e751807cc9b1c308
Flavio <flavio.fernandes1200@gmail.com>
2022-08-24 15:52:05 -0300

    First object-relational mapping in user class

--- backend/src/main/java/com/flavio/sbook/domain/model/User.java
+++ backend/src/main/java/com/flavio/sbook/domain/model/User.java
@@ -4,14 +4,26 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.*;
+
 @Getter
 @Setter
-@EqualsAndHashCode
-
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
 public class User {
 
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
     private Long id;
+
+    @Column(name = "user_name")
     private String firstName;
+
+    @Column(name = "user_email")
     private String email;
+
+    @Column(name = "user_password")
     private String password;
 }
