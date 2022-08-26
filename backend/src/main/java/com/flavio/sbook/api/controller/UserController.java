7afa5a98d5af02e6377ae51a9c870ec3e2381a55
Flavio <flavio.fernandes1200@gmail.com>
2022-08-26 12:37:53 -0300

    UserRepository

--- backend/src/main/java/com/flavio/sbook/api/controller/UserController.java
+++ backend/src/main/java/com/flavio/sbook/api/controller/UserController.java
@@ -1,21 +1,32 @@
 package com.flavio.sbook.api.controller;
 
 import com.flavio.sbook.domain.model.User;
+import com.flavio.sbook.domain.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
 
-    @PersistenceContext
-    private EntityManager entityManager;
+    @Autowired
+    private UserRepository userRepository;
 
-    @GetMapping("/users")
+    @GetMapping
     public List<User> getAll() {
-        return  entityManager.createQuery("from User", User.class).getResultList();
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getById(@PathVariable Long id) {
+        return userRepository.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
 }
---
+++ backend/src/main/java/com/flavio/sbook/domain/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.flavio.sbook.domain.repository;
+
+import com.flavio.sbook.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
