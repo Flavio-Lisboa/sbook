5af9d011794c9d4b50f03098dbbb26d07ef7f0c9
Flavio <flavio.fernandes1200@gmail.com>
2022-08-26 13:23:38 -0300

    SaveUser

--- backend/src/main/java/com/flavio/sbook/api/controller/UserController.java
+++ backend/src/main/java/com/flavio/sbook/api/controller/UserController.java
@@ -3,11 +3,9 @@ package com.flavio.sbook.api.controller;
 import com.flavio.sbook.domain.model.User;
 import com.flavio.sbook.domain.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,5 +26,11 @@ public class UserController {
         return userRepository.findById(id)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public User saveUser(@RequestBody User user) {
+        return userRepository.save(user);
     }
 }
